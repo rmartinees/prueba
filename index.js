598c3eb314dd7e9c26cde902569f323988d48115
Ricardo Martinez <ricardo@ricardadas.com>
2022-01-31 16:25:20 +0100

    ga

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ express()
 	  let vientos = [ "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSO", "SO", "OSO", "O", "ONO", "NO", "NNO"  ];
 	 
 	  for (let i = 0; i < result.rows.length; i++) {
-		result.rows[i].dirviento = vientos[result.rows[i].dirviento - process.env.TIMES];
+		result.rows[i].dirviento = vientos[result.rows[i].dirviento-process.env.TIMES];
  	  }
 	  if (result.rows.length)
          result.rows[0].campo=process.env.CAMPO;
